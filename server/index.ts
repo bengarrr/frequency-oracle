5c25698af61bd20fe4b507abdf2457f2d3d20198
bengarrr <kevindwaite@gmail.com>
2022-10-13 15:04:10 -0700

    query service

--- server/index.ts
+++ server/index.ts
@@ -45,7 +45,7 @@ fastify.get('/', async (request, reply) => {
     reply.send(payload);
 });
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
