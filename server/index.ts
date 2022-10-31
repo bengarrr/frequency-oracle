e956649e619de9dbeb6679d1bf9b981d644fe5a4
bengarrr <kevindwaite@gmail.com>
2022-10-31 14:38:00 -0700

    fixed price calculations

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,8 @@ dotenv.config({ path: `.env.${process.env.NODE || 'development'}` });
 import Fastify from "fastify";
 import sql from "./sql.service";
 
+import * as scraper from "./scrape";
+
 const tradepairs = ['SCRT/USD', 'BTC/USD', 'ETH/USD', 'OSMO/ATOM'];
 const exchanges = require('../exchanges.json');
 
@@ -11,33 +13,49 @@ const fastify = Fastify({
     logger: true
 });
 
-fastify.get('/', async (request, reply) => {
+fastify.get('/ohlcv', async (request, reply) => {
     const { date, exchange, symbol, interval } = request.query as any;
 
     const json = await query(date, exchange, symbol, interval);
 
-    const averaged_market_price = json.reduce((prevPriceSum, curr) => {
+    const averaged_market_price = json!.reduce((prevPriceSum, curr) => {
         const sum_closing_price = curr.json[4] + prevPriceSum;
         return sum_closing_price;
     }, 0) / interval;
 
-    const last_timestamp = json[interval-1][0];
+    const last_timestamp = json![interval-1][0];
     const payload = { json, averaged_market_price, next: last_timestamp };
     reply.send(payload);
     return;
-
-    reply.send(payload);
 });
+
+fastify.get('/', async (request, reply) => {
+    if(request.query) {
+        const { timestamp } = request.query as any;
+        const price = await scraper.getPrice(timestamp);
+        return reply.send({ price });
+    }
+    const price = await scraper.getPrice();
+    reply.send({ price });
+})
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
     }
-    fastify.log.info(`Server is now listening on ${address}`);
+
+    scraper.start().then(_ => {
+        fastify.log.info(`Scraper has been intialized`);
+    });
+
+    setInterval(async () => {
+        await scraper.restart();
+        fastify.log.info(`Scraper has been restarted`);
+    }, 60000);
 })
 
-async function query(date, exchange = "kraken", interval = 30, symbol = "SCRT/USD") {
+async function query(date: string, exchange: string = "kraken", interval: number = 30, symbol: string = "SCRT/USD") {
     const table = exchange+'_'+'ohlcv';
 
     if(date) {
---
+++ server/scrape.ts
@@ -0,0 +1,70 @@
+import * as puppeteer from "puppeteer";
+
+const endpoint = 'https://coinmarketcap.com/currencies/secret/';
+
+let browser: puppeteer.Browser | null = null;
+let page: puppeteer.Page | null = null;
+
+function wait(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function diffDays(t1: number, t2: number) {
+    const diff = Math.abs(t1 - t2);
+    const hours = Math.ceil(diff / (1000 * 60 * 60));
+    const days = Math.round(diff / (1000 * 60 * 60 * 24));
+    return { hours, days };
+}
+
+export async function start() {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto(endpoint);
+
+    return Promise.resolve();
+}
+
+export async function restart() {
+    await browser?.close();
+    await wait(100);
+    await start();
+
+    return Promise.resolve();
+}
+
+export async function reload() {
+    await page?.close();
+    page = await browser!.newPage();
+    await page.goto(endpoint);
+}
+
+export async function getPriceHistorical(timestamp: number) {
+    // const endpoint = 'https://www.coindesk.com/price/secret/';
+    const tabsSelector = '.react-tabs__tabs-list';
+    const chartSelector = '.tv-lightweight-charts canvas';
+    const page = await browser!.newPage();
+    await page.goto(endpoint);
+    await page.waitForSelector(chartSelector);
+
+    const diff =  diffDays(Date.now(), timestamp);
+    if(diff.days < 1) {
+        const cursor = diff.hours / 24 * 100;
+    }
+    if(diff.days > 7) {
+        const cursor = diff.days / 30 * 100;
+    }
+    const cursor = diff.days / 7 * 100;
+}
+
+export async function getPrice(timestamp?: number) {
+    const priceSelector = '.top-summary-container .priceSection .priceValue';
+
+    await page?.waitForSelector(priceSelector);
+    const price = await page?.evaluate(priceSelector => {
+        return document.querySelector(priceSelector)?.querySelector('span')?.innerText;
+    }, priceSelector)
+
+    return Promise.resolve(price);
+}
